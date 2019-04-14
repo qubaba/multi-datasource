8f03dee1b7ff254047b26ecead553b053b2718a4
qp <2055294353@qq.com>
2019-04-14 18:53:48 +0800

    spring 多数据源解决方案

---
+++ druid-datasource/src/main/java/com/xiaoqu/druid/DruidSpringApplication.java
@@ -0,0 +1,13 @@
+package com.xiaoqu.druid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DruidSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DruidSpringApplication.class, args);
+    }
+
+}
